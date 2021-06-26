235b20b3346e87fe8af6b8fdaeac62eae84e4541
tani <berukoko1220-blog@yahoo.co.jp>
2021-06-26 22:58:11 +0900

    🔧 リフトアップする必要がなくなったので修正

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,9 @@
 import 'tailwindcss/tailwind.css'
 
 import { AppProps } from 'next/app'
-import { useInputText } from 'src/hooks/useInputText'
 
 const App = ({ Component, pageProps }: AppProps) => {
-  const InputText = useInputText()
-  return <Component {...pageProps} {...InputText} />
+  return <Component {...pageProps} />
 }
 
 export default App
