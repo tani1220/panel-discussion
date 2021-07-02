22706440e5815d87f35affd951dfae7dfc5b567b
tani <berukoko1220-blog@yahoo.co.jp>
2021-07-02 23:36:28 +0900

    🎨 動的ページをスタイリング

--- src/pages/tasks/[id].tsx
+++ src/pages/tasks/[id].tsx
@@ -1,4 +1,6 @@
 import type { GetStaticPaths, GetStaticProps, InferGetStaticPropsType, NextPage } from 'next'
+import Link from 'next/link'
+import { Button } from 'src/components/Button'
 import { getTaskData, getTaskIds } from 'src/lib/getTaskData'
 
 type Props = InferGetStaticPropsType<typeof getStaticProps>
@@ -6,10 +8,17 @@ type Props = InferGetStaticPropsType<typeof getStaticProps>
 const TaskPage: NextPage<Props> = ({ initialData }) => {
   console.log(initialData)
   return (
-    <div>
-      {initialData.question}
-      {initialData.name}
-    </div>
+    <>
+      <div className="h-screen flex border-8 border-indigo-600">
+        <div className="m-auto p-20">
+          <h1 className="text-5xl">{initialData.question}</h1>
+          <h2 className="text-3xl mt-5">{initialData.name}</h2>
+          <Link href="/">
+            <Button className="float-right">戻る</Button>
+          </Link>
+        </div>
+      </div>
+    </>
   )
 }
 
