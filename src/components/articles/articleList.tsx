45eba24525b6b1ae1bea97e520e3e25285a6ac0a
tani <berukoko1220-blog@yahoo.co.jp>
2021-07-06 21:40:15 +0900

    ⚡️ 不必要のデータ取得を削除、またarticlesファイルの名前変更

---
+++ src/components/articles/articleList.tsx
@@ -0,0 +1,38 @@
+import { db } from 'firebase/clientApp'
+import { useEffect, useState, VFC } from 'react'
+import { TextCard } from 'src/components/TextCard'
+import type { articlePost } from 'src/types/types'
+
+type Props = {
+  newTasks: articlePost
+}
+
+export const Article: VFC<Props> = (props) => {
+  const [articles, setArticles] = useState(props.newTasks)
+
+  useEffect(() => {
+    const unsubscribe = db
+      .collection('tasks')
+      .orderBy('createdAt')
+      .onSnapshot((snapshot) => {
+        setArticles(snapshot.docs.map((doc) => ({ id: doc.id, question: doc.data().question })))
+      })
+    return () => unsubscribe()
+  }, [])
+
+  return (
+    <div>
+      <ul>
+        {articles.map((task) => (
+          <li className="pt-4 text-3xl" key={task.id}>
+            <div className="cursor-pointer">
+              <TextCard id={task.id}>
+                <a>{task.question}</a>
+              </TextCard>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
