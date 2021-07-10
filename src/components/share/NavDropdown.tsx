e654fc104886ebdbd6768520ee1e8a677d6b22d3
tani <berukoko1220-blog@yahoo.co.jp>
2021-07-10 23:14:05 +0900

    🎨 ヘッダーレスポンシブ対応

---
+++ src/components/share/NavDropdown.tsx
@@ -0,0 +1,27 @@
+import Link from 'next/link'
+import { VFC } from 'react'
+import { AddArticleForm } from 'src/components/articles/addArticleFrom'
+import { Button } from 'src/components/share/Button'
+import { Logout } from 'src/components/share/Logout'
+
+type Props = {
+  isOpen: boolean
+}
+
+export const NavDropdown: VFC<Props> = (props) => {
+  return (
+    <div>
+      {props.isOpen ? (
+        <div className="text-white bg-black flex justify-between p-4 px-6 border-t-2 sm:hidden">
+          <AddArticleForm />
+
+          <Link href="/login">
+            <Button className="mr-3">管理者</Button>
+          </Link>
+
+          <Logout />
+        </div>
+      ) : null}
+    </div>
+  )
+}
