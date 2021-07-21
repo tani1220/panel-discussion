c980b94aa89d0a185dac9e58392dac6c498d79d0
tani <berukoko1220-blog@yahoo.co.jp>
2021-07-21 22:58:40 +0900

    ♻️ リファクタ

--- src/components/share/Container.tsx
+++ src/components/share/Container.tsx
@@ -11,12 +11,14 @@ type Props = HeaderProps & {
 export const Container: VFC<Props> = (props) => {
   const { children, ...headerProps } = props
   return (
-    <div className="flex">
-      <div className="h-screen overflow-scroll w-full bg-black bg-opacity-90">
-        <Header {...headerProps} />
-        <div className="mx-auto py-16 sm:py-20 ">{children}</div>
+    <div>
+      <Header {...headerProps} />
+      <div className="flex h-screen pt-16">
+        <div className="overflow-scroll w-full bg-black bg-opacity-90">
+          <div className="mx-auto pb-16 pt-2">{children}</div>
+        </div>
+        <SlideNav />
       </div>
-      <SlideNav />
     </div>
   )
 }
