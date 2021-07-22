24b9faef9f92c22ae5491d1f3f08272e14f8474e
tani <berukoko1220-blog@yahoo.co.jp>
2021-07-22 22:48:48 +0900

    ✨ チャット投稿機能追加

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,27 +1,53 @@
+import { ChevronRightIcon } from '@heroicons/react/solid'
 import { useCallback, useState } from 'react'
 
 export const Chat = () => {
-  const [chatText, setChatText] = useState()
+  const [chatText, setChatText] = useState('')
+  const [chatList, setChatList] = useState<string[]>([])
 
   const hundleText = useCallback(
     (e: any) => {
-      const value = e.target.value
-      setChatText(value)
+      setChatText(e.target.value)
     },
     [chatText]
   )
 
+  const hundleAdd = useCallback(() => {
+    setChatList((prevChatList) => {
+      return [...prevChatList, chatText]
+    })
+  }, [chatText])
+
   return (
-    <div className="mb-4">
+    <div className="mb-4 bg-gray-600">
+      <ul className="text-white">
+        {chatList.map((item) => {
+          return (
+            <li key={item} className="py-1 text-sm">
+              {item}
+            </li>
+          )
+        })}
+      </ul>
       <div className="mt-2 text-white">
         <input
           type="text"
           name="text"
           value={chatText}
           onChange={hundleText}
-          className="focus:ring-indigo-500 focus:border-indigo-500 w-full h-8 px-2 sm:text-sm bg-gray-900 border-gray-300"
+          autoComplete="off"
+          className="focus:outline-none w-full h-8 px-2 sm:text-sm bg-white bg-opacity-20 border-gray-300 rounded"
           placeholder="テキストを入力"
         />
+      </div>
+      <div className="flex justify-end pt-3">
+        <div className="text-xs my-auto pr-3">文字数</div>
+        <button onClick={hundleAdd}>
+          <ChevronRightIcon
+            className="hover:bg-gray-700 rounded-full text-white flex-shrink-0 h-8 w-8"
+            aria-hidden="true"
+          />
+        </button>
       </div>
     </div>
   )
