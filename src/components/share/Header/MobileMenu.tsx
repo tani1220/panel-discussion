673ac781afa1e72768b8c5aa03caef7054f50cf5
tani <berukoko1220-blog@yahoo.co.jp>
2021-08-03 14:45:57 +0900

    🎨 MobileMenu修正

--- src/components/share/Header/MobileMenu.tsx
+++ src/components/share/Header/MobileMenu.tsx
@@ -1,25 +1,49 @@
+import { Menu, Transition } from '@headlessui/react'
+import { MenuIcon } from '@heroicons/react/solid'
 import Link from 'next/link'
-import { VFC } from 'react'
-import { ArticleForm } from 'src/components/articles/ArticleForm'
+import { Fragment } from 'react'
 import { Button } from 'src/components/share/Button'
 
 import { Logout } from './Logout'
-import { NavItemProps } from './types'
 
-export const MobileMenu: VFC<NavItemProps> = (props) => {
+export const MobileMenu = () => {
   return (
-    <div>
-      {props.isOpen ? (
-        <div className="text-white bg-black flex justify-between p-4 px-6 border-t-2 lg:hidden">
-          {props.right === 'after' ? <ArticleForm roomId={props.roomId} /> : null}
+    <Menu as="div" className="relative flex text-left">
+      {({ open }) => (
+        <>
+          <Menu.Button className="inline-flex justify-center">
+            <MenuIcon className="h-7 w-7" aria-hidden="true" />
+          </Menu.Button>
 
-          <Link href="/login">
-            <Button className="mr-3">管理者</Button>
-          </Link>
+          <Transition
+            show={open}
+            as={Fragment}
+            enter="transition ease-out duration-100"
+            enterFrom="transform opacity-0 scale-95"
+            enterTo="transform opacity-100 scale-100"
+            leave="transition ease-in duration-75"
+            leaveFrom="transform opacity-100 scale-100"
+            leaveTo="transform opacity-0 scale-95"
+          >
+            <Menu.Items
+              static
+              className="mt-10 origin-top-right absolute right-0 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
+            >
+              <div className="py-1">
+                <Menu.Item>
+                  <Link href="/login">
+                    <Button className="mr-3">管理者</Button>
+                  </Link>
+                </Menu.Item>
 
-          <Logout />
-        </div>
-      ) : null}
-    </div>
+                <Menu.Item>
+                  <Logout />
+                </Menu.Item>
+              </div>
+            </Menu.Items>
+          </Transition>
+        </>
+      )}
+    </Menu>
   )
 }
