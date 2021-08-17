7f41a0e18e7f73c48bd25038902d04915e56efce
tani <berukoko1220-blog@yahoo.co.jp>
2021-08-17 21:35:02 +0900

    ⚡️ Toastを使用、また UserDialogをリファクタ

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,9 +1,24 @@
 import 'tailwindcss/tailwind.css'
 
 import { AppProps } from 'next/app'
+import { Toaster } from 'react-hot-toast'
 
 const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Component {...pageProps} />
+      <Toaster
+        containerClassName="font-mono"
+        toastOptions={{
+          duration: 4000,
+          style: {
+            background: '#666666',
+            color: 'white',
+          },
+        }}
+      />
+    </div>
+  )
 }
 
 export default App
